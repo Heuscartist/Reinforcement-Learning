a66d66924c4d53602a0c01e2b2cf1e56141b7c09
Adil Mubashir Chaudhry <75829014+Heuscartist@users.noreply.github.com>
2024-04-12 23:43:45 +0500

    Cliffwalking Updates

---
+++ Cliff Walking/sarsa.py
@@ -0,0 +1,69 @@
+import gym
+import numpy as np
+import pickle as pkl
+import os
+
+cliffEnv = gym.make("CliffWalking-v0", render_mode="human") #create an environment from the available in gym
+#cliffEnv = gym.make("CliffWalking-v0")
+
+if os.path.exists("sarsa_q_table.pkl"):
+    # Load the q_table from the pickle file
+    q_table = pkl.load(open("sarsa_q_table.pkl", "rb"))
+else:
+    q_table = np.zeros(shape=(48, 4)) # 48 states and 4 actions
+
+# We are defining an epsilon greedy policy here
+# Epsilon greedy policy is a way to balance exploration and exploitation
+# We are choosing a random action with probability epsilon and 
+# choosing the best action with probability 1-epsilon
+def policy(state, explore=0.0): # epsilon greedy policy
+    
+    # choose action based on epsilon greedy policy
+    # q value of state-action pairs returning the most optimal action
+    action = int(np.argmax(q_table[state]))
+    
+    # if random number is less than epsilon, choose random action
+    if np.random.random() <= explore:
+        action = int(np.random.randint(low=0, high=4, size=1))
+    return action
+
+# Parameters
+EPSILON = 0.1
+ALPHA = 0.1
+GAMMA = 0.9
+NUM_EPISODES = 500
+
+
+for episode in range(NUM_EPISODES):
+    total_reward = 0
+    episode_length = 0
+    done = False
+
+    state = cliffEnv.reset()
+    #if episode == 0:
+    action = policy(state[0], EPSILON) # choose action based on policy
+    #else:
+    #    action = policy(state, EPSILON) # choose action based on policy
+
+    while not done:
+        # print(cliffEnv.render())
+        next_state, reward, done, _, _ = cliffEnv.step(action)
+        next_action = policy(next_state, EPSILON) # choose next action based on policy
+
+        # update q table
+        if episode_length == 0:
+            q_table[state[0]][action] += ALPHA * (reward + GAMMA +q_table[next_state][next_action] - q_table[state[0]][action])
+        else:
+            q_table[state][action] += ALPHA * (reward + GAMMA +q_table[next_state][next_action] - q_table[state][action])
+
+        state = next_state
+        action = next_action
+        total_reward += reward
+        episode_length += 1
+    print("Episode: {}, Total Reward: {}, Episode Length: {}".format(episode, total_reward, episode_length))
+
+
+cliffEnv.close()
+
+pkl.dump(q_table, open("sarsa_q_table.pkl", "wb"))
+print("Training Complete. Q Table Saved")
